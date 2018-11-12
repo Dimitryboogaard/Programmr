d75283a61d1ab53730d491adb9c65d9428db363b
Dimitry <40239514+Dimitryboogaard@users.noreply.github.com>
2018-11-12 18:53:25 +0100

    Add files via upload

---
+++ 25 Matrix multiplication/25MatrixMultiplication/25MatrixMultiplication/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace _25MatrixMultiplication
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.rtAntwoord = new System.Windows.Forms.RichTextBox();
+            this.btnAntwoord = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // rtAntwoord
+            // 
+            this.rtAntwoord.Location = new System.Drawing.Point(40, 27);
+            this.rtAntwoord.Name = "rtAntwoord";
+            this.rtAntwoord.Size = new System.Drawing.Size(100, 96);
+            this.rtAntwoord.TabIndex = 0;
+            this.rtAntwoord.Text = "";
+            // 
+            // btnAntwoord
+            // 
+            this.btnAntwoord.Location = new System.Drawing.Point(40, 129);
+            this.btnAntwoord.Name = "btnAntwoord";
+            this.btnAntwoord.Size = new System.Drawing.Size(75, 23);
+            this.btnAntwoord.TabIndex = 1;
+            this.btnAntwoord.Text = "Antwoord";
+            this.btnAntwoord.UseVisualStyleBackColor = true;
+            this.btnAntwoord.Click += new System.EventHandler(this.btnAntwoord_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(216, 206);
+            this.Controls.Add(this.btnAntwoord);
+            this.Controls.Add(this.rtAntwoord);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox rtAntwoord;
+        private System.Windows.Forms.Button btnAntwoord;
+    }
+}
+
---
+++ 25 Matrix multiplication/25MatrixMultiplication/25MatrixMultiplication/Form1.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _25MatrixMultiplication
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            arrayMatrix1[0, 0] = 1;
+            arrayMatrix1[0, 1] = 4;
+            arrayMatrix1[0, 2] = 7;
+            arrayMatrix1[1, 0] = 2;
+            arrayMatrix1[1, 1] = 5;
+            arrayMatrix1[1, 2] = 8;
+            arrayMatrix1[2, 0] = 3;
+            arrayMatrix1[2, 1] = 6;
+            arrayMatrix1[2, 2] = 1;
+
+            arrayMatrix2[0, 0] = 1;
+            arrayMatrix2[0, 1] = 3;
+            arrayMatrix2[0, 2] = 4;
+            arrayMatrix2[1, 0] = 2;
+            arrayMatrix2[1, 1] = 2;
+            arrayMatrix2[1, 2] = 5;
+            arrayMatrix2[2, 0] = 3;
+            arrayMatrix2[2, 1] = 1;
+            arrayMatrix2[2, 2] = 2;
+        }
+
+        int[,] arrayMatrix2 = new int[3, 3];
+        int[,] arrayMatrix1 = new int[3, 3];
+
+        int intTeller, intTeller2, intTeller3, intUitkomst;
+
+        private void btnAntwoord_Click(object sender, EventArgs e)
+        {
+            for (intTeller = 0; intTeller <= 2; intTeller++)
+            {
+                intUitkomst += arrayMatrix1[intTeller, 0] * arrayMatrix2[0, intTeller];
+            }
+            rtAntwoord.Text += intUitkomst.ToString() + " ";
+            intUitkomst = 0;
+
+            for (intTeller = 0; intTeller <= 2; intTeller++)
+            {
+                intUitkomst += arrayMatrix1[intTeller, 0] * arrayMatrix2[1, intTeller];
+            }
+            rtAntwoord.Text += intUitkomst.ToString() + " ";
+            intUitkomst = 0;
+
+            for (intTeller = 0; intTeller <= 2; intTeller++)
+            {
+                intUitkomst += arrayMatrix1[intTeller, 0] * arrayMatrix2[2, intTeller];
+            }
+            rtAntwoord.Text += intUitkomst.ToString() + Environment.NewLine;
+            intUitkomst = 0;
+
+            for (intTeller = 0; intTeller <= 2; intTeller++)
+            {
+                intUitkomst += arrayMatrix1[intTeller, 1] * arrayMatrix2[0, intTeller];
+            }
+            rtAntwoord.Text += intUitkomst.ToString() + " ";
+            intUitkomst = 0;
+
+            for (intTeller = 0; intTeller <= 2; intTeller++)
+            {
+                intUitkomst += arrayMatrix1[intTeller, 1] * arrayMatrix2[1, intTeller];
+            }
+            rtAntwoord.Text += intUitkomst.ToString() + " ";
+            intUitkomst = 0;
+
+            for (intTeller = 0; intTeller <= 2; intTeller++)
+            {
+                intUitkomst += arrayMatrix1[intTeller, 1] * arrayMatrix2[2, intTeller];
+            }
+            rtAntwoord.Text += intUitkomst.ToString() + Environment.NewLine;
+            intUitkomst = 0;
+
+            for (intTeller = 0; intTeller <= 2; intTeller++)
+            {
+                intUitkomst += arrayMatrix1[intTeller, 2] * arrayMatrix2[0, intTeller];
+            }
+            rtAntwoord.Text += intUitkomst.ToString() + " ";
+            intUitkomst = 0;
+
+            for (intTeller = 0; intTeller <= 2; intTeller++)
+            {
+                intUitkomst += arrayMatrix1[intTeller, 2] * arrayMatrix2[1, intTeller];
+            }
+            rtAntwoord.Text += intUitkomst.ToString() + " ";
+            intUitkomst = 0;
+
+            for (intTeller = 0; intTeller <= 2; intTeller++)
+            {
+                intUitkomst += arrayMatrix1[intTeller, 2] * arrayMatrix2[2, intTeller];
+            }
+            rtAntwoord.Text += intUitkomst.ToString() + " ";
+            intUitkomst = 0;
+
+
+        }
+
+
+
+    }
+}
