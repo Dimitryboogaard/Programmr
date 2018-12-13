3bb97621f29712c6a642ca92a5a797d7fddb14f5
Dimitry <40239514+Dimitryboogaard@users.noreply.github.com>
2018-12-13 19:50:04 +0100

    Add files via upload

---
+++ 36 Ulam sequence/36UlamSequence/36UlamSequence/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _36UlamSequence
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int intInvoer, intUitvoer;
+
+        private void btnAntwoord_Click(object sender, EventArgs e)
+        {
+            intInvoer = Convert.ToInt32(tbInvoer.Text);
+
+            do
+            {
+                if(intInvoer % 2 == 0)
+                {
+                    Even();
+                }
+
+                else
+                {
+                    Oneven();
+                }
+
+                if (intInvoer != 1)
+                {
+                    rtUitvoer.Text += intInvoer.ToString() + " -> ";
+                }
+                else
+                {
+                    rtUitvoer.Text += intInvoer.ToString() ;
+
+                }
+            } while (intInvoer != 1);
+
+        }
+
+        void Even()
+        {
+            intInvoer = intInvoer / 2;
+            return;
+        }
+
+        void Oneven()
+        {
+            intInvoer = intInvoer * 3 + 1;
+        }
+    }
+}
---
+++ 36 Ulam sequence/36UlamSequence/36UlamSequence/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("36UlamSequence")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("36UlamSequence")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("87a03b1b-2fd3-45b5-a88c-fc2919402828")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
