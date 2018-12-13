1f964852b8c0bee80db449f95caac8e01fdd8b80
Dimitry <40239514+Dimitryboogaard@users.noreply.github.com>
2018-12-13 20:04:33 +0100

    Add files via upload

---
+++ 62 Find day of given date/62FindDayOfGivenDate/62FindDayOfGivenDate/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace _62FindDayOfGivenDate
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbInvoer = new System.Windows.Forms.TextBox();
+            this.lblInvoer = new System.Windows.Forms.Label();
+            this.btnAntwoord = new System.Windows.Forms.Button();
+            this.lblWelkeDag = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // tbInvoer
+            // 
+            this.tbInvoer.Location = new System.Drawing.Point(73, 31);
+            this.tbInvoer.Name = "tbInvoer";
+            this.tbInvoer.Size = new System.Drawing.Size(100, 20);
+            this.tbInvoer.TabIndex = 0;
+            // 
+            // lblInvoer
+            // 
+            this.lblInvoer.AutoSize = true;
+            this.lblInvoer.Location = new System.Drawing.Point(30, 34);
+            this.lblInvoer.Name = "lblInvoer";
+            this.lblInvoer.Size = new System.Drawing.Size(37, 13);
+            this.lblInvoer.TabIndex = 1;
+            this.lblInvoer.Text = "Invoer";
+            // 
+            // btnAntwoord
+            // 
+            this.btnAntwoord.Location = new System.Drawing.Point(73, 57);
+            this.btnAntwoord.Name = "btnAntwoord";
+            this.btnAntwoord.Size = new System.Drawing.Size(75, 23);
+            this.btnAntwoord.TabIndex = 2;
+            this.btnAntwoord.Text = "Antwoord";
+            this.btnAntwoord.UseVisualStyleBackColor = true;
+            this.btnAntwoord.Click += new System.EventHandler(this.btnAntwoord_Click);
+            // 
+            // lblWelkeDag
+            // 
+            this.lblWelkeDag.AutoSize = true;
+            this.lblWelkeDag.Location = new System.Drawing.Point(70, 83);
+            this.lblWelkeDag.Name = "lblWelkeDag";
+            this.lblWelkeDag.Size = new System.Drawing.Size(59, 13);
+            this.lblWelkeDag.TabIndex = 3;
+            this.lblWelkeDag.Text = "Welke dag";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(322, 235);
+            this.Controls.Add(this.lblWelkeDag);
+            this.Controls.Add(this.btnAntwoord);
+            this.Controls.Add(this.lblInvoer);
+            this.Controls.Add(this.tbInvoer);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbInvoer;
+        private System.Windows.Forms.Label lblInvoer;
+        private System.Windows.Forms.Button btnAntwoord;
+        private System.Windows.Forms.Label lblWelkeDag;
+    }
+}
+
---
+++ 62 Find day of given date/62FindDayOfGivenDate/62FindDayOfGivenDate/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _62FindDayOfGivenDate
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        DateTime dteDatum = new DateTime();
+        int intDayOfWeek;
+        string strDayOfWeek;
+
+
+        private void btnAntwoord_Click(object sender, EventArgs e)
+        {
+            dteDatum = Convert.ToDateTime(tbInvoer.Text);
+
+            intDayOfWeek = (int)dteDatum.DayOfWeek;
+
+            switch(intDayOfWeek)
+            {
+                case 0:
+                    strDayOfWeek = "zondag";
+                    break;
+
+                case 1:
+                    strDayOfWeek = "maandag";
+                    break;
+
+                case 2:
+                    strDayOfWeek = "dinsdag";
+                    break;
+
+                case 3:
+                    strDayOfWeek = "woensdag";
+                    break;
+
+                case 4:
+                    strDayOfWeek = "donderdag";
+                    break;
+
+                case 5:
+                    strDayOfWeek = "vrijdag";
+                    break;
+
+                case 6:
+                    strDayOfWeek = "zaterdag";
+                    break;
+                    
+            }
+
+            lblWelkeDag.Text = "Die datum val op een " + strDayOfWeek + ".";
+        }
+    }
+}
