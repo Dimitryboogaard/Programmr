b763eb4f9f710bceef83452bf5d11f5bd1faec6c
Dimitry <40239514+Dimitryboogaard@users.noreply.github.com>
2018-12-13 19:57:06 +0100

    Add files via upload

---
+++ 48 Find number is consecutive or not/48FindNumberIsConsecutive/48FindNumberIsConsecutive/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace _48FindNumberIsConsecutive
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbInvoer = new System.Windows.Forms.TextBox();
+            this.lblInvoer = new System.Windows.Forms.Label();
+            this.btnAntwoord = new System.Windows.Forms.Button();
+            this.lblOpeenVolgendOfNiet = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // tbInvoer
+            // 
+            this.tbInvoer.Location = new System.Drawing.Point(70, 25);
+            this.tbInvoer.Name = "tbInvoer";
+            this.tbInvoer.Size = new System.Drawing.Size(100, 20);
+            this.tbInvoer.TabIndex = 0;
+            // 
+            // lblInvoer
+            // 
+            this.lblInvoer.AutoSize = true;
+            this.lblInvoer.Location = new System.Drawing.Point(27, 28);
+            this.lblInvoer.Name = "lblInvoer";
+            this.lblInvoer.Size = new System.Drawing.Size(37, 13);
+            this.lblInvoer.TabIndex = 1;
+            this.lblInvoer.Text = "Invoer";
+            // 
+            // btnAntwoord
+            // 
+            this.btnAntwoord.Location = new System.Drawing.Point(70, 51);
+            this.btnAntwoord.Name = "btnAntwoord";
+            this.btnAntwoord.Size = new System.Drawing.Size(75, 23);
+            this.btnAntwoord.TabIndex = 2;
+            this.btnAntwoord.Text = "Antwoord";
+            this.btnAntwoord.UseVisualStyleBackColor = true;
+            this.btnAntwoord.Click += new System.EventHandler(this.btnAntwoord_Click);
+            // 
+            // lblOpeenVolgendOfNiet
+            // 
+            this.lblOpeenVolgendOfNiet.AutoSize = true;
+            this.lblOpeenVolgendOfNiet.Location = new System.Drawing.Point(67, 77);
+            this.lblOpeenVolgendOfNiet.Name = "lblOpeenVolgendOfNiet";
+            this.lblOpeenVolgendOfNiet.Size = new System.Drawing.Size(83, 13);
+            this.lblOpeenVolgendOfNiet.TabIndex = 3;
+            this.lblOpeenVolgendOfNiet.Text = "Opeenvolgend?";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(340, 270);
+            this.Controls.Add(this.lblOpeenVolgendOfNiet);
+            this.Controls.Add(this.btnAntwoord);
+            this.Controls.Add(this.lblInvoer);
+            this.Controls.Add(this.tbInvoer);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbInvoer;
+        private System.Windows.Forms.Label lblInvoer;
+        private System.Windows.Forms.Button btnAntwoord;
+        private System.Windows.Forms.Label lblOpeenVolgendOfNiet;
+    }
+}
+
---
+++ 48 Find number is consecutive or not/48FindNumberIsConsecutive/48FindNumberIsConsecutive/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _48FindNumberIsConsecutive
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        string strInvoer;
+        int intStringlengte, intTeller;
+        bool booOpeenVolgend = true;
+
+        private void btnAntwoord_Click(object sender, EventArgs e)
+        {
+            strInvoer = tbInvoer.Text;
+            intStringlengte = strInvoer.Length;
+
+            for(intTeller = 0; intTeller < intStringlengte - 1; intTeller++)
+            {
+                if (Convert.ToInt16(strInvoer.Substring(intTeller, 1)) != Convert.ToInt16(strInvoer.Substring(intTeller + 1, 1)) - 1)
+                {
+                    booOpeenVolgend = false;
+                }
+
+            }
+
+            if(booOpeenVolgend)
+            {
+                lblOpeenVolgendOfNiet.Text = "Getal is opeenvolgend";
+            }
+            
+            else if(!booOpeenVolgend)
+            {
+                lblOpeenVolgendOfNiet.Text = "Getal is niet opeenvolgend";
+            }
+        }
+    }
+}
