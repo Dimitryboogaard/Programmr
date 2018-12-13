ae31ea76a1a0f56242d17bf1eeac56187e05422e
Dimitry <40239514+Dimitryboogaard@users.noreply.github.com>
2018-12-13 19:59:02 +0100

    Add files via upload

---
+++ 52 Armstrong number/52ArmstrongNumber/52ArmstrongNumber/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace _52ArmstrongNumber
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbInvoer = new System.Windows.Forms.TextBox();
+            this.lblInvoer = new System.Windows.Forms.Label();
+            this.lblArmstrongOrNot = new System.Windows.Forms.Label();
+            this.btnAntwoord = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // tbInvoer
+            // 
+            this.tbInvoer.Location = new System.Drawing.Point(99, 29);
+            this.tbInvoer.Name = "tbInvoer";
+            this.tbInvoer.Size = new System.Drawing.Size(100, 20);
+            this.tbInvoer.TabIndex = 0;
+            // 
+            // lblInvoer
+            // 
+            this.lblInvoer.AutoSize = true;
+            this.lblInvoer.Location = new System.Drawing.Point(56, 32);
+            this.lblInvoer.Name = "lblInvoer";
+            this.lblInvoer.Size = new System.Drawing.Size(37, 13);
+            this.lblInvoer.TabIndex = 1;
+            this.lblInvoer.Text = "Invoer";
+            // 
+            // lblArmstrongOrNot
+            // 
+            this.lblArmstrongOrNot.AutoSize = true;
+            this.lblArmstrongOrNot.Location = new System.Drawing.Point(96, 81);
+            this.lblArmstrongOrNot.Name = "lblArmstrongOrNot";
+            this.lblArmstrongOrNot.Size = new System.Drawing.Size(84, 13);
+            this.lblArmstrongOrNot.TabIndex = 2;
+            this.lblArmstrongOrNot.Text = "Armstrong or not";
+            // 
+            // btnAntwoord
+            // 
+            this.btnAntwoord.Location = new System.Drawing.Point(99, 55);
+            this.btnAntwoord.Name = "btnAntwoord";
+            this.btnAntwoord.Size = new System.Drawing.Size(75, 23);
+            this.btnAntwoord.TabIndex = 3;
+            this.btnAntwoord.Text = "Antwoord";
+            this.btnAntwoord.UseVisualStyleBackColor = true;
+            this.btnAntwoord.Click += new System.EventHandler(this.btnAntwoord_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(355, 263);
+            this.Controls.Add(this.btnAntwoord);
+            this.Controls.Add(this.lblArmstrongOrNot);
+            this.Controls.Add(this.lblInvoer);
+            this.Controls.Add(this.tbInvoer);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbInvoer;
+        private System.Windows.Forms.Label lblInvoer;
+        private System.Windows.Forms.Label lblArmstrongOrNot;
+        private System.Windows.Forms.Button btnAntwoord;
+    }
+}
+
---
+++ 52 Armstrong number/52ArmstrongNumber/52ArmstrongNumber/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _52ArmstrongNumber
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int intTeller, intStringLengte;
+        double dblTijdelijk, dblAntwoord;
+        string strInvoer;
+
+        private void btnAntwoord_Click(object sender, EventArgs e)
+        {
+            strInvoer = tbInvoer.Text;
+            intStringLengte = strInvoer.Length;
+
+            for(intTeller = 0; intTeller < intStringLengte; intTeller++)
+            {
+                dblTijdelijk = Convert.ToDouble(strInvoer.Substring(intTeller, 1));
+                dblAntwoord += Math.Pow(dblTijdelijk, 3);
+            }
+
+            if(dblAntwoord == Convert.ToDouble(strInvoer))
+            {
+                lblArmstrongOrNot.Text = "Number is Armstrong";
+
+            }
+
+            else
+            {
+                lblArmstrongOrNot.Text = "Number is not Armstrong";
+            }
+        }
+    }
+}
