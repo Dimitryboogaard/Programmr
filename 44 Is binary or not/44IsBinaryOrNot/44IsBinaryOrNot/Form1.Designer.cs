38893f8c5f5705b374e5a2b5f467f53056b59944
Dimitry <40239514+Dimitryboogaard@users.noreply.github.com>
2018-12-13 19:55:38 +0100

    Add files via upload

---
+++ 44 Is binary or not/44IsBinaryOrNot/44IsBinaryOrNot/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace _44IsBinaryOrNot
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbInvoer = new System.Windows.Forms.TextBox();
+            this.lblInvoer = new System.Windows.Forms.Label();
+            this.btnAntwoord = new System.Windows.Forms.Button();
+            this.lblBinaryOrNot = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // tbInvoer
+            // 
+            this.tbInvoer.Location = new System.Drawing.Point(77, 35);
+            this.tbInvoer.Name = "tbInvoer";
+            this.tbInvoer.Size = new System.Drawing.Size(100, 20);
+            this.tbInvoer.TabIndex = 0;
+            // 
+            // lblInvoer
+            // 
+            this.lblInvoer.AutoSize = true;
+            this.lblInvoer.Location = new System.Drawing.Point(34, 38);
+            this.lblInvoer.Name = "lblInvoer";
+            this.lblInvoer.Size = new System.Drawing.Size(37, 13);
+            this.lblInvoer.TabIndex = 1;
+            this.lblInvoer.Text = "Invoer";
+            // 
+            // btnAntwoord
+            // 
+            this.btnAntwoord.Location = new System.Drawing.Point(77, 61);
+            this.btnAntwoord.Name = "btnAntwoord";
+            this.btnAntwoord.Size = new System.Drawing.Size(75, 23);
+            this.btnAntwoord.TabIndex = 2;
+            this.btnAntwoord.Text = "Antwoord";
+            this.btnAntwoord.UseVisualStyleBackColor = true;
+            this.btnAntwoord.Click += new System.EventHandler(this.btnAntwoord_Click);
+            // 
+            // lblBinaryOrNot
+            // 
+            this.lblBinaryOrNot.AutoSize = true;
+            this.lblBinaryOrNot.Location = new System.Drawing.Point(74, 87);
+            this.lblBinaryOrNot.Name = "lblBinaryOrNot";
+            this.lblBinaryOrNot.Size = new System.Drawing.Size(66, 13);
+            this.lblBinaryOrNot.TabIndex = 3;
+            this.lblBinaryOrNot.Text = "Binary or not";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(380, 260);
+            this.Controls.Add(this.lblBinaryOrNot);
+            this.Controls.Add(this.btnAntwoord);
+            this.Controls.Add(this.lblInvoer);
+            this.Controls.Add(this.tbInvoer);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbInvoer;
+        private System.Windows.Forms.Label lblInvoer;
+        private System.Windows.Forms.Button btnAntwoord;
+        private System.Windows.Forms.Label lblBinaryOrNot;
+    }
+}
+
---
+++ 44 Is binary or not/44IsBinaryOrNot/44IsBinaryOrNot/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _44IsBinaryOrNot
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int intTeller, intStringLengte;
+        string strInvoer;
+        bool booNotBinary = false;
+
+        private void btnAntwoord_Click(object sender, EventArgs e)
+        {
+            strInvoer = tbInvoer.Text;
+            intStringLengte = strInvoer.Length;
+
+            for(intTeller = 0; intTeller < intStringLengte; intTeller++)
+            {
+                if(strInvoer.Substring(intTeller, 1) != "1" && strInvoer.Substring(intTeller, 1) != "0")
+                {
+                    booNotBinary = true;
+                }
+            }
+
+            if(booNotBinary)
+            {
+                lblBinaryOrNot.Text = "Not binary";
+            }
+
+            else if(!booNotBinary)
+            {
+                lblBinaryOrNot.Text = "Binary";
+            }
+        }
+    }
+}
