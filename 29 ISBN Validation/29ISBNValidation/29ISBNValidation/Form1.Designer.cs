e6ebfd37b979668d0848fb83ba5f38736252db5e
Dimitry <40239514+Dimitryboogaard@users.noreply.github.com>
2018-12-13 19:41:30 +0100

    Add files via upload

---
+++ 29 ISBN Validation/29ISBNValidation/29ISBNValidation/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace _29ISBNValidation
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbInvoer = new System.Windows.Forms.TextBox();
+            this.btnCheck = new System.Windows.Forms.Button();
+            this.lblValidOrNot = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // tbInvoer
+            // 
+            this.tbInvoer.Location = new System.Drawing.Point(89, 63);
+            this.tbInvoer.Name = "tbInvoer";
+            this.tbInvoer.Size = new System.Drawing.Size(100, 20);
+            this.tbInvoer.TabIndex = 0;
+            // 
+            // btnCheck
+            // 
+            this.btnCheck.Location = new System.Drawing.Point(89, 89);
+            this.btnCheck.Name = "btnCheck";
+            this.btnCheck.Size = new System.Drawing.Size(75, 23);
+            this.btnCheck.TabIndex = 1;
+            this.btnCheck.Text = "Check";
+            this.btnCheck.UseVisualStyleBackColor = true;
+            this.btnCheck.Click += new System.EventHandler(this.btnCheck_Click);
+            // 
+            // lblValidOrNot
+            // 
+            this.lblValidOrNot.AutoSize = true;
+            this.lblValidOrNot.Location = new System.Drawing.Point(86, 140);
+            this.lblValidOrNot.Name = "lblValidOrNot";
+            this.lblValidOrNot.Size = new System.Drawing.Size(60, 13);
+            this.lblValidOrNot.TabIndex = 2;
+            this.lblValidOrNot.Text = "Valid or not";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(378, 247);
+            this.Controls.Add(this.lblValidOrNot);
+            this.Controls.Add(this.btnCheck);
+            this.Controls.Add(this.tbInvoer);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbInvoer;
+        private System.Windows.Forms.Button btnCheck;
+        private System.Windows.Forms.Label lblValidOrNot;
+    }
+}
+
---
+++ 29 ISBN Validation/29ISBNValidation/29ISBNValidation/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _29ISBNValidation
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int intStringLengte, intTeller, intAntwoord;
+
+        private void btnCheck_Click(object sender, EventArgs e)
+        {
+            intStringLengte = tbInvoer.Text.Length;
+
+            if(intStringLengte != 10)
+            {
+                lblValidOrNot.Text = "Not valid";
+            }
+
+            else if(intStringLengte == 10)
+            {
+                for(intTeller = 0; intTeller < 10; intTeller++)
+                {
+                    intAntwoord += (intTeller + 1) * Convert.ToInt16(tbInvoer.Text.Substring(intTeller, 1));
+                }
+
+                if(intAntwoord % 11 == 0)
+                {
+                    lblValidOrNot.Text = "Valid";
+                }
+
+                else
+                {
+                    lblValidOrNot.Text = "Not valid";
+                }
+            }
+        }
+    }
+}
