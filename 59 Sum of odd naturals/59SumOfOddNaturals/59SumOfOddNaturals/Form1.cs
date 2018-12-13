c76f4ed7d6e5fa6bee0aaba8b3702af1d1011c0b
Dimitry <40239514+Dimitryboogaard@users.noreply.github.com>
2018-12-13 20:02:51 +0100

    Add files via upload

---
+++ 59 Sum of odd naturals/59SumOfOddNaturals/59SumOfOddNaturals/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _59SumOfOddNaturals
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int intTeller, intInvoer, intAntwoord;
+
+        private void btnAntwoord_Click(object sender, EventArgs e)
+        {
+            intInvoer = Convert.ToInt32(tbInvoer.Text);
+
+            for(intTeller = 1; intTeller <= intInvoer; intTeller++)
+            {
+                if(intTeller % 2 != 0)
+                {
+                    intAntwoord += intTeller;
+                }
+
+            }
+
+            tbUitvoer.Text = intAntwoord.ToString();
+        }
+    }
+}
