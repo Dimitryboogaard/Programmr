41ed56700ef3553cc68e0c4317422dfe2468ff5f
Dimitry <40239514+Dimitryboogaard@users.noreply.github.com>
2018-12-13 19:42:11 +0100

    Add files via upload

---
+++ 30 Triangular star/30TriangularStar/30TriangularStar/Form1.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace _30TriangularStar
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.rtUitvoer = new System.Windows.Forms.RichTextBox();
+            this.tbInvoer = new System.Windows.Forms.TextBox();
+            this.btnAntwoord = new System.Windows.Forms.Button();
+            this.lblOnevenOfNiet = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // rtUitvoer
+            // 
+            this.rtUitvoer.Location = new System.Drawing.Point(263, 40);
+            this.rtUitvoer.Name = "rtUitvoer";
+            this.rtUitvoer.Size = new System.Drawing.Size(180, 214);
+            this.rtUitvoer.TabIndex = 0;
+            this.rtUitvoer.Text = "";
+            // 
+            // tbInvoer
+            // 
+            this.tbInvoer.Location = new System.Drawing.Point(75, 40);
+            this.tbInvoer.Name = "tbInvoer";
+            this.tbInvoer.Size = new System.Drawing.Size(100, 20);
+            this.tbInvoer.TabIndex = 1;
+            // 
+            // btnAntwoord
+            // 
+            this.btnAntwoord.Location = new System.Drawing.Point(75, 66);
+            this.btnAntwoord.Name = "btnAntwoord";
+            this.btnAntwoord.Size = new System.Drawing.Size(75, 23);
+            this.btnAntwoord.TabIndex = 2;
+            this.btnAntwoord.Text = "Antwoord";
+            this.btnAntwoord.UseVisualStyleBackColor = true;
+            this.btnAntwoord.Click += new System.EventHandler(this.btnAntwoord_Click);
+            // 
+            // lblOnevenOfNiet
+            // 
+            this.lblOnevenOfNiet.AutoSize = true;
+            this.lblOnevenOfNiet.Location = new System.Drawing.Point(72, 92);
+            this.lblOnevenOfNiet.Name = "lblOnevenOfNiet";
+            this.lblOnevenOfNiet.Size = new System.Drawing.Size(0, 13);
+            this.lblOnevenOfNiet.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(495, 365);
+            this.Controls.Add(this.lblOnevenOfNiet);
+            this.Controls.Add(this.btnAntwoord);
+            this.Controls.Add(this.tbInvoer);
+            this.Controls.Add(this.rtUitvoer);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox rtUitvoer;
+        private System.Windows.Forms.TextBox tbInvoer;
+        private System.Windows.Forms.Button btnAntwoord;
+        private System.Windows.Forms.Label lblOnevenOfNiet;
+    }
+}
+
---
+++ 30 Triangular star/30TriangularStar/30TriangularStar/Form1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _30TriangularStar
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int intInvoer, intTeller, intStringLengte, intTellerSterretje, intTellerStreepje;
+
+        private void btnAntwoord_Click(object sender, EventArgs e)
+        {
+            intInvoer = Convert.ToInt32(tbInvoer.Text);
+            intTellerStreepje = intInvoer;
+            if (intInvoer % 2 != 0)
+            {
+                lblOnevenOfNiet.Text = "";
+                do
+                {
+                    intTellerSterretje++;
+                    intTellerStreepje = intTellerStreepje - 2;
+
+                    for (intTeller = 1; intTeller <= intTellerSterretje; intTeller++)
+                    {
+                        rtUitvoer.Text += "*";
+                    }
+
+                    for (intTeller = 1; intTeller <= intTellerStreepje; intTeller++)
+                    {
+                        rtUitvoer.Text += "-";
+                    }
+
+                    for (intTeller = 1; intTeller <= intTellerSterretje; intTeller++)
+                    {
+                        rtUitvoer.Text += "*";
+                    }
+
+                    rtUitvoer.Text += Environment.NewLine;
+
+                } while (intTellerStreepje != 1);
+
+                for (intTeller = 1; intTeller <= intInvoer; intTeller++)
+                {
+                    rtUitvoer.Text += "*";
+                }
+
+                rtUitvoer.Text += Environment.NewLine;
+
+                do
+                {
+
+                    for (intTeller = 1; intTeller <= intTellerSterretje; intTeller++)
+                    {
+                        rtUitvoer.Text += "*";
+                    }
+
+                    for (intTeller = 1; intTeller <= intTellerStreepje; intTeller++)
+                    {
+                        rtUitvoer.Text += "-";
+                    }
+
+                    for (intTeller = 1; intTeller <= intTellerSterretje; intTeller++)
+                    {
+                        rtUitvoer.Text += "*";
+                    }
+
+                    rtUitvoer.Text += Environment.NewLine;
+
+                    intTellerStreepje += 2;
+                    intTellerSterretje--;
+
+                } while (intTellerStreepje < intInvoer );
+
+            }
+
+            else
+            {
+                tbInvoer.Text = "";
+                lblOnevenOfNiet.Text = "Oneven nummer invullen.";
+            }
+        }
+    }
+}
