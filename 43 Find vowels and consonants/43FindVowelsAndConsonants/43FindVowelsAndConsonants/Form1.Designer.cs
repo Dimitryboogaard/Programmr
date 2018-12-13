3eecc99595c568061c5e1383d81d94388a925cd3
Dimitry <40239514+Dimitryboogaard@users.noreply.github.com>
2018-12-13 19:54:56 +0100

    Add files via upload

---
+++ 43 Find vowels and consonants/43FindVowelsAndConsonants/43FindVowelsAndConsonants/Form1.Designer.cs
@@ -0,0 +1,194 @@
+﻿namespace _43FindVowelsAndConsonants
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbInvoer = new System.Windows.Forms.TextBox();
+            this.lblInvoer = new System.Windows.Forms.Label();
+            this.tbHoofdLetterMedeKlinkers = new System.Windows.Forms.TextBox();
+            this.lblHoofdLetterMedeKlinkers = new System.Windows.Forms.Label();
+            this.tbKleineLettersMedeKlinkers = new System.Windows.Forms.TextBox();
+            this.lblKleineLettersMedeKlinkers = new System.Windows.Forms.Label();
+            this.tbHoofdLetterKlinkers = new System.Windows.Forms.TextBox();
+            this.lblHoofdLetterKlinkers = new System.Windows.Forms.Label();
+            this.tbKleineLetterKlinkers = new System.Windows.Forms.TextBox();
+            this.lblKleineLetterKlinkers = new System.Windows.Forms.Label();
+            this.tbAantalKlinkers = new System.Windows.Forms.TextBox();
+            this.lblAantalKlinkers = new System.Windows.Forms.Label();
+            this.btnAntwoord = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // tbInvoer
+            // 
+            this.tbInvoer.Location = new System.Drawing.Point(66, 32);
+            this.tbInvoer.Name = "tbInvoer";
+            this.tbInvoer.Size = new System.Drawing.Size(271, 20);
+            this.tbInvoer.TabIndex = 0;
+            // 
+            // lblInvoer
+            // 
+            this.lblInvoer.AutoSize = true;
+            this.lblInvoer.Location = new System.Drawing.Point(23, 35);
+            this.lblInvoer.Name = "lblInvoer";
+            this.lblInvoer.Size = new System.Drawing.Size(37, 13);
+            this.lblInvoer.TabIndex = 1;
+            this.lblInvoer.Text = "Invoer";
+            // 
+            // tbHoofdLetterMedeKlinkers
+            // 
+            this.tbHoofdLetterMedeKlinkers.Location = new System.Drawing.Point(147, 81);
+            this.tbHoofdLetterMedeKlinkers.Name = "tbHoofdLetterMedeKlinkers";
+            this.tbHoofdLetterMedeKlinkers.Size = new System.Drawing.Size(100, 20);
+            this.tbHoofdLetterMedeKlinkers.TabIndex = 2;
+            // 
+            // lblHoofdLetterMedeKlinkers
+            // 
+            this.lblHoofdLetterMedeKlinkers.AutoSize = true;
+            this.lblHoofdLetterMedeKlinkers.Location = new System.Drawing.Point(17, 84);
+            this.lblHoofdLetterMedeKlinkers.Name = "lblHoofdLetterMedeKlinkers";
+            this.lblHoofdLetterMedeKlinkers.Size = new System.Drawing.Size(124, 13);
+            this.lblHoofdLetterMedeKlinkers.TabIndex = 3;
+            this.lblHoofdLetterMedeKlinkers.Text = "Hoofdletter medeklinkers";
+            // 
+            // tbKleineLettersMedeKlinkers
+            // 
+            this.tbKleineLettersMedeKlinkers.Location = new System.Drawing.Point(147, 107);
+            this.tbKleineLettersMedeKlinkers.Name = "tbKleineLettersMedeKlinkers";
+            this.tbKleineLettersMedeKlinkers.Size = new System.Drawing.Size(100, 20);
+            this.tbKleineLettersMedeKlinkers.TabIndex = 4;
+            // 
+            // lblKleineLettersMedeKlinkers
+            // 
+            this.lblKleineLettersMedeKlinkers.AutoSize = true;
+            this.lblKleineLettersMedeKlinkers.Location = new System.Drawing.Point(9, 110);
+            this.lblKleineLettersMedeKlinkers.Name = "lblKleineLettersMedeKlinkers";
+            this.lblKleineLettersMedeKlinkers.Size = new System.Drawing.Size(132, 13);
+            this.lblKleineLettersMedeKlinkers.TabIndex = 5;
+            this.lblKleineLettersMedeKlinkers.Text = "Kleine letters medeklinkers";
+            // 
+            // tbHoofdLetterKlinkers
+            // 
+            this.tbHoofdLetterKlinkers.Location = new System.Drawing.Point(147, 133);
+            this.tbHoofdLetterKlinkers.Name = "tbHoofdLetterKlinkers";
+            this.tbHoofdLetterKlinkers.Size = new System.Drawing.Size(100, 20);
+            this.tbHoofdLetterKlinkers.TabIndex = 6;
+            // 
+            // lblHoofdLetterKlinkers
+            // 
+            this.lblHoofdLetterKlinkers.AutoSize = true;
+            this.lblHoofdLetterKlinkers.Location = new System.Drawing.Point(43, 136);
+            this.lblHoofdLetterKlinkers.Name = "lblHoofdLetterKlinkers";
+            this.lblHoofdLetterKlinkers.Size = new System.Drawing.Size(98, 13);
+            this.lblHoofdLetterKlinkers.TabIndex = 7;
+            this.lblHoofdLetterKlinkers.Text = "Hoofdletter klinkers";
+            // 
+            // tbKleineLetterKlinkers
+            // 
+            this.tbKleineLetterKlinkers.Location = new System.Drawing.Point(147, 159);
+            this.tbKleineLetterKlinkers.Name = "tbKleineLetterKlinkers";
+            this.tbKleineLetterKlinkers.Size = new System.Drawing.Size(100, 20);
+            this.tbKleineLetterKlinkers.TabIndex = 8;
+            // 
+            // lblKleineLetterKlinkers
+            // 
+            this.lblKleineLetterKlinkers.AutoSize = true;
+            this.lblKleineLetterKlinkers.Location = new System.Drawing.Point(40, 162);
+            this.lblKleineLetterKlinkers.Name = "lblKleineLetterKlinkers";
+            this.lblKleineLetterKlinkers.Size = new System.Drawing.Size(101, 13);
+            this.lblKleineLetterKlinkers.TabIndex = 9;
+            this.lblKleineLetterKlinkers.Text = "Kleine letter klinkers";
+            // 
+            // tbAantalKlinkers
+            // 
+            this.tbAantalKlinkers.Location = new System.Drawing.Point(147, 185);
+            this.tbAantalKlinkers.Name = "tbAantalKlinkers";
+            this.tbAantalKlinkers.Size = new System.Drawing.Size(100, 20);
+            this.tbAantalKlinkers.TabIndex = 10;
+            // 
+            // lblAantalKlinkers
+            // 
+            this.lblAantalKlinkers.AutoSize = true;
+            this.lblAantalKlinkers.Location = new System.Drawing.Point(65, 188);
+            this.lblAantalKlinkers.Name = "lblAantalKlinkers";
+            this.lblAantalKlinkers.Size = new System.Drawing.Size(76, 13);
+            this.lblAantalKlinkers.TabIndex = 11;
+            this.lblAantalKlinkers.Text = "Aantal klinkers";
+            // 
+            // btnAntwoord
+            // 
+            this.btnAntwoord.Location = new System.Drawing.Point(66, 58);
+            this.btnAntwoord.Name = "btnAntwoord";
+            this.btnAntwoord.Size = new System.Drawing.Size(75, 23);
+            this.btnAntwoord.TabIndex = 12;
+            this.btnAntwoord.Text = "Antwoord";
+            this.btnAntwoord.UseVisualStyleBackColor = true;
+            this.btnAntwoord.Click += new System.EventHandler(this.btnAntwoord_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(399, 260);
+            this.Controls.Add(this.btnAntwoord);
+            this.Controls.Add(this.lblAantalKlinkers);
+            this.Controls.Add(this.tbAantalKlinkers);
+            this.Controls.Add(this.lblKleineLetterKlinkers);
+            this.Controls.Add(this.tbKleineLetterKlinkers);
+            this.Controls.Add(this.lblHoofdLetterKlinkers);
+            this.Controls.Add(this.tbHoofdLetterKlinkers);
+            this.Controls.Add(this.lblKleineLettersMedeKlinkers);
+            this.Controls.Add(this.tbKleineLettersMedeKlinkers);
+            this.Controls.Add(this.lblHoofdLetterMedeKlinkers);
+            this.Controls.Add(this.tbHoofdLetterMedeKlinkers);
+            this.Controls.Add(this.lblInvoer);
+            this.Controls.Add(this.tbInvoer);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbInvoer;
+        private System.Windows.Forms.Label lblInvoer;
+        private System.Windows.Forms.TextBox tbHoofdLetterMedeKlinkers;
+        private System.Windows.Forms.Label lblHoofdLetterMedeKlinkers;
+        private System.Windows.Forms.TextBox tbKleineLettersMedeKlinkers;
+        private System.Windows.Forms.Label lblKleineLettersMedeKlinkers;
+        private System.Windows.Forms.TextBox tbHoofdLetterKlinkers;
+        private System.Windows.Forms.Label lblHoofdLetterKlinkers;
+        private System.Windows.Forms.TextBox tbKleineLetterKlinkers;
+        private System.Windows.Forms.Label lblKleineLetterKlinkers;
+        private System.Windows.Forms.TextBox tbAantalKlinkers;
+        private System.Windows.Forms.Label lblAantalKlinkers;
+        private System.Windows.Forms.Button btnAntwoord;
+    }
+}
+
---
+++ 43 Find vowels and consonants/43FindVowelsAndConsonants/43FindVowelsAndConsonants/Form1.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _43FindVowelsAndConsonants
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            arrayLetters[0] = "A";
+            arrayLetters[1] = "E";
+            arrayLetters[2] = "I";
+            arrayLetters[3] = "O";
+            arrayLetters[4] = "U";
+            arrayLetters[5] = "F";
+            arrayLetters[6] = "G";
+            arrayLetters[7] = "H";
+            arrayLetters[8] = "C";
+            arrayLetters[9] = "J";
+            arrayLetters[10] = "K";
+            arrayLetters[11] = "L";
+            arrayLetters[12] = "M";
+            arrayLetters[13] = "N";
+            arrayLetters[14] = "D";
+            arrayLetters[15] = "P";
+            arrayLetters[16] = "Q";
+            arrayLetters[17] = "R";
+            arrayLetters[18] = "S";
+            arrayLetters[19] = "T";
+            arrayLetters[20] = "B";
+            arrayLetters[21] = "V";
+            arrayLetters[22] = "W";
+            arrayLetters[23] = "X";
+            arrayLetters[24] = "Y";
+            arrayLetters[25] = "Z";
+            arrayLetters[26] = "a";
+            arrayLetters[27] = "e";
+            arrayLetters[28] = "i";
+            arrayLetters[29] = "o";
+            arrayLetters[30] = "u";
+            arrayLetters[31] = "f";
+            arrayLetters[32] = "g";
+            arrayLetters[33] = "h";
+            arrayLetters[34] = "c";
+            arrayLetters[35] = "j";
+            arrayLetters[36] = "k";
+            arrayLetters[37] = "l";
+            arrayLetters[38] = "m";
+            arrayLetters[39] = "n";
+            arrayLetters[40] = "d";
+            arrayLetters[41] = "p";
+            arrayLetters[42] = "q";
+            arrayLetters[43] = "r";
+            arrayLetters[44] = "s";
+            arrayLetters[45] = "t";
+            arrayLetters[46] = "b";
+            arrayLetters[47] = "w";
+            arrayLetters[48] = "v";
+            arrayLetters[49] = "x";
+            arrayLetters[50] = "y";
+            arrayLetters[51] = "z";
+        }
+
+        string[] arrayLetters = new string[100];
+        string strInvoer, strSubstring;
+        int intStringLengte, intTeller, intTeller2, intHoofdLetterMedeKlinker, intHoofdLetterKlinker,
+            intKleineLetterMedeKlinker, intKleineLetterKlinker, intAantalKlinkers;
+
+        private void btnAntwoord_Click(object sender, EventArgs e)
+        {
+            strInvoer = tbInvoer.Text;
+            intStringLengte = strInvoer.Length;
+
+            for(intTeller = 0; intTeller < intStringLengte; intTeller++)
+            {
+                strSubstring = strInvoer.Substring(intTeller, 1);
+
+                for(intTeller2 = 0; intTeller2 <= 52; intTeller2++)
+                {
+                    if(arrayLetters[intTeller2] == strSubstring && intTeller2 < 5)
+                    {
+                        intHoofdLetterKlinker++;
+                        intAantalKlinkers++;
+                    }
+
+                    if(arrayLetters[intTeller2] == strSubstring && intTeller2 > 4 && intTeller2 < 26)
+                    {
+                        intHoofdLetterMedeKlinker++;
+                    }
+
+                    if(arrayLetters[intTeller2] == strSubstring && intTeller2 > 25 && intTeller2 < 31)
+                    {
+                        intKleineLetterKlinker++;
+                        intAantalKlinkers++;
+                    }
+
+                    if (arrayLetters[intTeller2] == strSubstring && intTeller2 > 30 && intTeller2 < 52)
+                    {
+                        intKleineLetterMedeKlinker++;
+                    }
+
+
+                }
+            }
+            tbHoofdLetterMedeKlinkers.Text = intHoofdLetterMedeKlinker.ToString();
+            tbHoofdLetterKlinkers.Text = intHoofdLetterKlinker.ToString();
+            tbKleineLetterKlinkers.Text = intKleineLetterKlinker.ToString();
+            tbKleineLettersMedeKlinkers.Text = intKleineLetterMedeKlinker.ToString();
+            tbAantalKlinkers.Text = intAantalKlinkers.ToString();
+        }
+    }
+}
