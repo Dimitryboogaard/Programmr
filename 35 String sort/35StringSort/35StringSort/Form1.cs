a065ede2bbf7f788a34ec1c4f8b00200c7a286ca
Dimitry <40239514+Dimitryboogaard@users.noreply.github.com>
2018-12-13 19:49:21 +0100

    Add files via upload

---
+++ 35 String sort/35StringSort/35StringSort/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _35StringSort
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        string strInvoer, strUitvoer;
+        int intTeller, intStringLengte;
+        string[] arrayInvoer = new string[1000];
+
+        private void btnAntwoord_Click(object sender, EventArgs e)
+        {
+            strInvoer = tbInvoer.Text;
+
+            intStringLengte = strInvoer.Length;
+
+            for(intTeller = 0; intTeller < intStringLengte; intTeller++)
+            {
+                arrayInvoer[intTeller] = strInvoer.Substring(intTeller, 1);
+            }
+
+            Array.Sort(arrayInvoer);
+
+            for(intTeller = 1000 - intStringLengte; intTeller < 1000; intTeller++)
+            {
+                tbUitvoer.Text += arrayInvoer[intTeller];
+            }
+        }
+    }
+}
